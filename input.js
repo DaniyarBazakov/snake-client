7a944393ec9bddc5bcb9d0a94d5feb29c8ebabc9
Daniyar Bazakov <bazakovd@gmail.com>
2024-06-11 17:42:41 -0400

    refactor handleUserInput in input.js

--- input.js
+++ input.js
@@ -1,5 +1,12 @@
 let connection;
 
+const movementCommands = {
+  'w': 'up',
+  'a': 'left',
+  's': 'down',
+  'd': 'right'
+}
+
 const setupInput = function (conn) {
   connection = conn;
   const stdin = process.stdin;
@@ -11,16 +18,12 @@ const setupInput = function (conn) {
 };
 
 const handleUserInput = function (key) {
+  const command = movementCommands[key];
   if (key === "\u0003") {
     process.exit();
-  } else if (key === "w") {
-    connection.write("Move: up");
-  } else if (key === "a") {
-    connection.write("Move: left");
-  } else if (key === "s") {
-    connection.write("Move: down");
-  } else if (key === "d") {
-    connection.write("Move: right");
+  }
+  if (command) {
+    connection.write(`Move: ${command}`);
   }
 };
 
