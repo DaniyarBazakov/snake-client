ec74bc62f780c7de8fa22a3896bc592d80d39d78
Daniyar Bazakov <bazakovd@gmail.com>
2024-06-11 16:48:20 -0400

    send message from client to server

--- client.js
+++ client.js
@@ -8,6 +8,14 @@ const connect = function() {
 
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server")
+  })
+
+  conn.on("connect", () => {
+    conn.write("Name: HYD");
+  });
+
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
